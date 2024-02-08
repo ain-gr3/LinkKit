db4ea44a30ad9cb2a6a487410bac95563fbf865b
Ain <ainobara@AinnoMacBook-Pro.local>
2024-02-08 13:19:23 +0900

    add: SampleProject

---
+++ Examples/LinkSample/LinkSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkSample
+//
+//  Created by Ain Obara on 2024/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
